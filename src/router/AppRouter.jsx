b297d8e0cfcd4c7cc82de1a1edd52070173a08f7
Yusuf <baliyusuf675@gmail.com>
2023-03-07 21:00:28 +0300

    app

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import { GlobalStyles } from "../styles/Global.styles";
+import Footer from "../components/footer/Footer";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import PrivateRouter from "./PrivateRouter";
+import Detail from "../pages/detail/Detail";
+import About from "../pages/about/About";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="/detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
