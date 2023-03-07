7b6cf6cec490411db230facae6c7ccf12b47a471
Yusuf <baliyusuf675@gmail.com>
2023-03-07 22:09:24 +0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Nav } from "./Navbar.style";
+import { Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 
 const Navbar = () => {
-  return <Nav justify="space-between">Navbar</Nav>;
+  return (
+    <Nav justify="space-between">
+      <Logo to="/">Clarus Library</Logo>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+      </Menu>
+    </Nav>
+  );
 };
 
 export default Navbar;
--- src/components/navbar/Navbar.style.jsx
+++ src/components/navbar/Navbar.style.jsx
@@ -1,4 +1,32 @@
 import styled from "styled-components";
 import Flex from "../../styles/Flex";
+import { NavLink } from "react-router-dom";
 
-export const Nav = styled(Flex)``;
+export const Nav = styled(Flex)`
+  background: ${({ theme }) => theme.colors.navbarBgColor};
+  padding: 1rem 2rem;
+`;
+
+export const Logo = styled(NavLink)`
+  padding: 1rem 0;
+  color: ${({ theme }) => theme.colors.logoColor};
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 2rem;
+`;
+
+export const Menu = styled(Flex)`
+  font-weight: bold;
+`;
+
+export const MenuLink = styled(NavLink)`
+  text-align: center;
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.linkColor};
+  transition: all 0.3s ease-in;
+  :hover {
+    color: ${({ theme }) => theme.colors.linkHoverColor};
+  }
+`;
