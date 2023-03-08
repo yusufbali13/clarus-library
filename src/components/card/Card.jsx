ba7d1a83fe9b1c803f7594e6816cea0cb523a1cb
Yusuf <baliyusuf675@gmail.com>
2023-03-08 21:58:08 +0300

    home

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { CardButton, CardContainer, CardHeader, CardImg } from "./Card.style";
+import defaultImg from "../../assets/book.jpg";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ item }) => {
+  return (
+    <CardContainer>
+      <CardHeader>{item.volumInfo.title}</CardHeader>
+      <CardImg src={item.volumInfo.imageLinks.smallThumbnail || defaultImg} />
+      <CardButton View More></CardButton>
+    </CardContainer>
+  );
 };
 
 export default Card;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,22 +2,47 @@ import React, { useState } from "react";
 import { HomeContainer, HomeImg, MainContainer } from "./Home.style";
 import Header from "../../components/header/Header";
 import homeImg from "../../assets/books.jpg";
+import axios from "axios";
+import Card from "../../components/card/Card";
 
 const Home = () => {
   // const printType = ["all","books","magazines"]
   const [query, setQuery] = useState("");
   const [selectType, setSelectType] = useState("all");
   const [myData, setMyData] = useState([]);
+  const APP_KEY = process.env.REACT_APP_apiKey;
+
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=${selectType}&key=${APP_KEY}`;
+
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(url);
+      console.log(data);
+      setMyData(data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <HomeContainer>
-      <Header setQuery={setQuery} setSelectType={setSelectType} />
+      <Header
+        setQuery={setQuery}
+        setSelectType={setSelectType}
+        getData={getData}
+      />
 
-      <HomeImg>
-        <img src={homeImg} alt="home image" />
-      </HomeImg>
-
-      <MainContainer></MainContainer>
+      {!myData.length ? (
+        <HomeImg>
+          <img src={homeImg} alt="home image" />
+        </HomeImg>
+      ) : (
+        <MainContainer wrap="wrap">
+          {myData.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </MainContainer>
+      )}
     </HomeContainer>
   );
 };
