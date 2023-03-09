88448d3a418c2c2362ca94fdc73da6ca2f2741b2
Yusuf <baliyusuf675@gmail.com>
2023-03-09 16:43:55 +0300

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -14,11 +14,11 @@ const About = () => {
     <AboutContainer>
       <ProfileImg src={profile} alt="anthony" />
       <InfoContainer>
-        <h2>Hi, I'm Anthony</h2>
-        <h3>I’m currently learning Full-Stack Development Languages.</h3>
+        <h2>Hi, I'm Yusuf</h2>
+        <h3>I’m currently learning Front-end Development Languages.</h3>
         <h4>
-          I've already known JS, ReactJS, NextJs, ReactNative, Django, NodeJS,
-          MongoDB,SQL, Python, AWS Services.
+          I've already known JS, ReactJS, NextJs, NodeJS, MongoDB,SQL, Python,
+          AWS Services, bootstrap, sass.
         </h4>
         <h4>💬 You can ask me anything you want to know</h4>
         <IconContainer>
