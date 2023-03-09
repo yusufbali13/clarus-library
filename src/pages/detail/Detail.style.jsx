a3cdc381219262c7dbcdec2fb7797e97fbaa7869
Yusuf <baliyusuf675@gmail.com>
2023-03-09 16:03:03 +0300

    detail

--- src/pages/detail/Detail.style.jsx
+++ src/pages/detail/Detail.style.jsx
@@ -11,7 +11,7 @@ export const DetailPart = styled(Flex)`
   border: 1px solid white;
   border-radius: 5px;
   background-color: ${({ theme }) => theme.colors.detailBgColor};
-  color: ${({ theme }) => theme.colors.detailColor}};
+  color: ${({ theme }) => theme.colors.detailColor};
   width: 80%;
 `;
 
